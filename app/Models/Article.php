a8a8c38f2fbcf0ed09471359ce28ce9698117776
Muneeb Usmani <muneebusmani8355@gmail.com>
2025-01-12 02:02:08 +0500

    v0.0.3 models implemented

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,9 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
     /** @use HasFactory<\Database\Factories\ArticleFactory> */
     use HasFactory;
+
+    /**
+     * Get the user that owns the Article
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get all of the pdfs for the Article
+     */
+    public function pdfs(): HasMany
+    {
+        return $this->hasMany(articles_pdfs::class);
+    }
+
+    protected $casts = [
+        'title' => 'string',
+        'description' => 'string',
+        'thumbnail' => 'string',
+    ];
 }
--- app/Models/articles_pdfs.php
+++ app/Models/articles_pdfs.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class articles_pdfs extends Model
 {
     /** @use HasFactory<\Database\Factories\ArticlesPdfsFactory> */
     use HasFactory;
+
+    /**
+     * Get the article that owns the articles_pdfs
+     */
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
 }
