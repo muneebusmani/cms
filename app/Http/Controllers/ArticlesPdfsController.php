7e70cf228d8a78e08a63f165e1c7d7cb60ae1487
Muneeb Usmani <muneebusmani8355@gmail.com>
2025-01-12 01:40:46 +0500

    v0.0.2 migrations implemented

---
+++ app/Http/Controllers/ArticlesPdfsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Storearticles_pdfsRequest;
+use App\Http\Requests\Updatearticles_pdfsRequest;
+use App\Models\articles_pdfs;
+
+class ArticlesPdfsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storearticles_pdfsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(articles_pdfs $articles_pdfs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(articles_pdfs $articles_pdfs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updatearticles_pdfsRequest $request, articles_pdfs $articles_pdfs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(articles_pdfs $articles_pdfs)
+    {
+        //
+    }
+}
---
+++ app/Models/articles_pdfs.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class articles_pdfs extends Model
+{
+    /** @use HasFactory<\Database\Factories\ArticlesPdfsFactory> */
+    use HasFactory;
+}
