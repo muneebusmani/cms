5915401cbe2583a0d59cb63158b6b8348b4f4cc5
Muneeb Usmani <muneebusmani8355@gmail.com>
2025-01-13 11:27:02 +0500

    incomplete commit, will work on it further

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'thumbnail' => 'required|file|image|mimes:png,jpg,jpeg,gif|extensions:png,jpg,jpeg,gif|max:2048',
+            'pdfs' => 'max:15',
+            'pdfs.*' => 'required|file|mimes:pdf|extensions:pdf|max:8192',
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        //
+    }
+}
